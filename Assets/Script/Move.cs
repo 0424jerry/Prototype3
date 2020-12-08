9b4d1fc407bf6acdacad27f931ba890212fb0bc5
0424jerry <jerrygithub@gms.ndhu.edu.tw>
2020-12-08 11:30:33 +0800

    First

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float speed = 10;
+    private PlayerController playerControllerScript;
+    private float leftB = -15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftB && gameObject.CompareTag("Prop_Barrier02"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
